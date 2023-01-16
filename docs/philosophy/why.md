7eab565ba09b418876134977dc5e93cf1e4d8fb4
intrnl <intrnl28+git@gmail.com>
2023-01-16 07:14:16 +0700

    chore: move stuff around

--- docs/philosophy/why.md
+++
@@ -1,5 +0,0 @@
----
-id: why
-title: Why
-sidebar_label: Why
----
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+	build: {
+		lib: {
+			entry: './lib/index.ts',
+			formats: ['esm', 'cjs'],
+			fileName: 'hooked-form',
+		},
+		rollupOptions: {
+			external: ['react', 'react/jsx-runtime'],
+		},
+	},
+	plugins: [
+		react(),
+		dts(),
+	],
+});
